d40878776051f53fc1333ee74f399f56249ef0a9
Wariha khan <150653055+warihakhan2@users.noreply.github.com>
2024-10-02 14:04:11 +0500

    Add files via upload

---
+++ Easy paisa app/app.js
@@ -0,0 +1,11 @@
+var email = "Wariha123@gmail.com";
+var password = 1234567890;
+var isLogedIn = false;
+if (email == "Wariha123@gmail.com" && password == 1234567890) {
+    console.log("Your email or password is incorrect");
+}
+else
+    (function (isLogedIn) {
+        if (isLogedIn === void 0) { isLogedIn = true; }
+        console.log("You are loged in");
+    });
---
+++ Easy paisa app/app.ts
@@ -0,0 +1,8 @@
+let email = "Wariha123@gmail.com";
+let password = 1234567890;
+let isLogedIn = false; 
+if(email == "Wariha123@gmail.com" && password == 1234567890){
+    console.log("Your email or password is incorrect");
+}else(isLogedIn = true) => {
+    console.log("You are loged in")
+}
